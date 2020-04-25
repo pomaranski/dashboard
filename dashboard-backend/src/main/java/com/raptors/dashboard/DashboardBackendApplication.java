f7f576aacbf04f0e7ade34a792f989b02ebdcf67
Paweł Pomarański <pawelpomaranski97@gmail.com>
2020-04-25 12:33:27 +0200

    init project

---
+++ dashboard-backend/src/main/java/com/raptors/dashboard/DashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.raptors.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardBackendApplication.class, args);
+	}
+
+}
