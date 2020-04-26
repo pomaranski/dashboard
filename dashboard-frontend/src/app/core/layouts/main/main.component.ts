948ce52fca3d411754f46193344cf8cc8ddb0964
Adam Lindner <ada.lin.97@gmail.com>
2020-04-26 11:39:06 +0200

    Init frontend project

---
+++ dashboard-frontend/src/app/core/layouts/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-main-layout',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+})
+export class MainComponent { }
